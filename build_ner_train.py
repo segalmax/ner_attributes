773bdf0a1fbeb9eee210dd3b35227212552812aa
ron-levi <58471340+ron-levi@users.noreply.github.com>
2020-07-20 21:09:23 +0300

    Add BiLSTMClassifier

--- build_ner_train.py
+++ build_ner_train.py
@@ -1,6 +1,7 @@
 import itertools
 import json
 import random
+from collections import Counter
 
 import pandas as pd
 import spacy
@@ -191,6 +192,84 @@ class SpacyClassifier(object):
         for ent in doc.ents:
             print ent.label_, ent.text
 
+def get_product_tokens_and_tags(product_doc, att_dict):
+    tokens, tags = [], []
+    for word in product_doc.strip().split():
+        tokens.append(word)
+        tag = [att if att_dict[att][0] == product_doc.index(word) else 'O' for att in att_dict.keys()][0]
+        tags.append(tag)
+    return tokens, tags
+
+
+class BiLSTMClassifier(object):
+    def __init__(self, df):
+        self.df = df
+        self.tokens, self.tags = self.prepare_input()
+
+    def prepare_input(self):
+        import ast
+        all_tokens = []
+        all_tags = []
+        self.df = self.df[self.df['attrs_indexes_dict'] != u'{}']
+        for _, row in self.df.iterrows():
+            product_doc = row['product_name']
+            att_dict = ast.literal_eval(row['attrs_indexes_dict'])
+            tokens, tags = get_product_tokens_and_tags(product_doc, att_dict)
+            all_tokens.append(tokens)
+            all_tags.append(tags)
+        return all_tokens, all_tags
+
+    # todo: can't import seaborn/matplotlib, apt-get install python-tk did not work; need to solve this for
+    #  visualisations
+    def sentence_length_distribution_analysis(self):
+        lengths = list(map(lambda x: len(x), self.df['product_name']))
+        ax = sns.distplot(lengths)
+        ax.set(xlabel="Number of tokens in a sentences", ylabel="% of sentences")
+        print "Median: ", np.median(lengths)
+        print "Average: ", round(np.mean(lengths), 2)
+
+    def summary(self):
+        flat_list = list(itertools.chain.from_iterable(self.tags))
+        cnt = Counter()
+        for word in flat_list:
+            cnt[word] += 1
+        count_dict = dict(cnt)
+        count_items = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
+        print("Number of unique items: ", len(count_items))
+        print("Average count: ", round(len(self.tags) / len(count_items)), "\n")
+        total_items = len(self.tags)
+        proportion_list = []
+        xlabels = []
+        for i, (key, value) in enumerate(count_items):
+            proportion = round(value * 100 / total_items, 2)
+            proportion_list.append(proportion)
+            xlabels.append(key)
+            # print(key, " ---> ", proportion, "%")
+        sns.set(style="whitegrid")
+        chart = sns.barplot(xlabels, proportion_list, orient="v")
+        plt.xlabel("Tokens/Tag")
+        plt.ylabel("Percentage of total Tokens/Tags")
+        chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+
+    # might be redundant since we know all words lengths are max 2-3 words
+    def average_entity_length(self):
+        tokens_tags = zip(self.tags, self.tokens)
+        entities_lengths = []
+        for tags, tokens in tokens_tags:
+            for ix, tag in enumerate(tags):
+                tag_dict = {tag: len(tokens[ix])}
+                entities_lengths.append(tag_dict)
+        flat_tags_list = list(itertools.chain.from_iterable(self.tags))
+        cnt = Counter()
+        for word in flat_tags_list:
+            cnt[word] += 1
+        count_dict = dict(cnt)
+        for tag in set(flat_tags_list):
+            total_length = 0
+        print 'test'
+
+    def tag_presence_percent(self):
+        pass
 
 def main():
     # fn = '/tmp/computer_1594228867.csv'
